e2623367abc83a4fed007e7221398f52c9f9bd30
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-07-12 01:36:38 +0530

    center the calculator in pricing.jsx

--- src/Pages/Pricing.jsx
+++ src/Pages/Pricing.jsx
@@ -5,10 +5,10 @@ import Calculator from '../Components/Calculator';
 
 const Pricing = () => {
   return (
-    <div className=''>
+    <div className='flex justify-center items-center w-full flex-col'>
     
       <div className="text-sky-950 text-2xl md:text-4xl text-center py-5">Calculate Your Shipping Price</div>
-      <div className="mx-6 md:mx-80 mb-5">
+      <div className="mx-6 mb-5">
       <Calculator/>
     </div>
     </div>

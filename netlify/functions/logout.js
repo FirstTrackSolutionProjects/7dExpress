dfa8c0d6cdbca3f0afa9f66fdc7d6c79f047cc21
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-07-20 14:56:28 +0530

    logout fix

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -2,10 +2,7 @@ exports.handler = async () => {
     return {
       statusCode: 200,
       headers: {
-        'Set-Cookie': [
-          'accessToken=; HttpOnly; Secure; Path=/; Max-Age=0',
-          'refreshToken=; HttpOnly; Secure; Path=/; Max-Age=0'
-        ]
+        'Set-Cookie': 'accessToken=; HttpOnly; Secure; Path=/; Max-Age=0'
       },
       body: JSON.stringify({ message: 'Logged out', success: true })
     };

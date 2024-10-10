cc93a0ea253bc83bf764a4c3d438b09ac7e2c064
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-10 12:47:47 +0530

    chore : redesigned the shipRocketCard

--- src/Components/Track.jsx
+++ src/Components/Track.jsx
@@ -3,126 +3,128 @@
 import { useState, useEffect } from "react"
 
 const API_URL = import.meta.env.VITE_APP_API_URL
-const Card = ({scan}) => {
-  return (
-      <>
-          <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
-              <div>{scan.ScanDateTime}</div>
-              <div>{scan.ScannedLocation}</div>
-              <div className="absolute right-8 cursor-pointer">{scan.Instructions}</div>
-          </div>
-      </>
-  )
-}
-const FlightGoCard = ({scan}) => {
-  return (
-      <>
-       <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
-              <div>{scan.event_at}</div>
-              <div>{scan.event_location}</div>
-              <div className="absolute right-8 cursor-pointer">{scan.event_description}</div>
-          </div>
-      </>
-  )
-}
-const MovinCard = ({scan}) => {
-  return (
-      <>
-       <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
-              <div>{scan.timestamp}</div>
-              <div className="absolute right-8 cursor-pointer">{scan.package_status}</div>
-          </div>
-      </>
-  )
-}
-const ShipRocketCard = ({scan}) => {
+const Card = ({ scan }) => {
   return (
     <>
-     <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
-              <div>{scan.timestamp}</div>
-              <div>{scan.location}</div>
-              <div className="absolute right-8 cursor-pointer">{scan.remarks}</div>
-          </div>
+      <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
+        <div>{scan.ScanDateTime}</div>
+        <div>{scan.ScannedLocation}</div>
+        <div className="absolute right-8 cursor-pointer">{scan.Instructions}</div>
+      </div>
     </>
-)
+  )
+}
+const FlightGoCard = ({ scan }) => {
+  return (
+    <>
+      <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
+        <div>{scan.event_at}</div>
+        <div>{scan.event_location}</div>
+        <div className="absolute right-8 cursor-pointer">{scan.event_description}</div>
+      </div>
+    </>
+  )
+}
+const MovinCard = ({ scan }) => {
+  return (
+    <>
+      <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
+        <div>{scan.timestamp}</div>
+        <div className="absolute right-8 cursor-pointer">{scan.package_status}</div>
+      </div>
+    </>
+  )
+}
+const ShipRocketCard = ({ scan }) => {
+  return (
+    <>
+      <div className="w-full py-3 bg-white relative items-center px-8 flex border-b space-x-4">
+        <div>
+          <div>{scan.remarks}</div>
+          <div>{scan.location}</div>
+          <div>{scan.timestamp}</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-const Result = ({data}) => {
+const Result = ({ data }) => {
   useEffect(() => {
-      console.log(data)
-  },[data])
+    console.log(data)
+  }, [data])
   return (
-      <>
-          <div className={`w-full p-8 overflow-hidden  `}>
-                  {data?.id == 1 ? data?.data.ShipmentData[0].Shipment.Scans.slice().reverse().map((scan, index)=>(
-                      <Card key={index} scan={scan.ScanDetail} />
-                  )) : null}
-                  {data?.id == 2 ? data?.data.docket_events.map((scan, index)=>(
-                      <FlightGoCard key={index} scan={scan} />
-                  )): null}
-                  {data?.id == 3 ? data?.data.map((scan, index)=>(
-                      <MovinCard key={index} scan={scan} />
-                  )): null}
-                  {data?.id == 4 ? data?.data.map((scan, index)=>(
-                      <ShipRocketCard key={index} scan={scan} />
-                  )): null}
-          </div>
-      
-      </>
+    <>
+      <div className={`w-full p-8 overflow-hidden  `}>
+        {data?.id == 1 ? data?.data.ShipmentData[0].Shipment.Scans.slice().reverse().map((scan, index) => (
+          <Card key={index} scan={scan.ScanDetail} />
+        )) : null}
+        {data?.id == 2 ? data?.data.docket_events.map((scan, index) => (
+          <FlightGoCard key={index} scan={scan} />
+        )) : null}
+        {data?.id == 3 ? data?.data.map((scan, index) => (
+          <MovinCard key={index} scan={scan} />
+        )) : null}
+        {data?.id == 4 ? data?.data.map((scan, index) => (
+          <ShipRocketCard key={index} scan={scan} />
+        )) : null}
+      </div>
+
+    </>
   )
 }
 
 const Track = () => {
-  const [formData,setFormData] = useState({
-    awb : ''
-})
+  const [formData, setFormData] = useState({
+    awb: ''
+  })
 
-useEffect(() => {
-    if (localStorage.getItem('track')){
-        setFormData({id: localStorage.getItem('track'), isWaybill: true})
-        localStorage.setItem('track','')
-        // handleSubmit(1)
+  useEffect(() => {
+    if (localStorage.getItem('track')) {
+      setFormData({ id: localStorage.getItem('track'), isWaybill: true })
+      localStorage.setItem('track', '')
+      // handleSubmit(1)
     }
-}, [])
+  }, [])
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]:type === 'radio' ? checked : value
+      [name]: type === 'radio' ? checked : value
     }));
   };
-const [trackingData,setTrackingData] = useState(null)
-const handleSubmit = async (e) => {
-    try{
-        e.preventDefault();
-    } catch (e) {}
+  const [trackingData, setTrackingData] = useState(null)
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+    } catch (e) { }
     const data = await fetch(`${API_URL}/track`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify(formData)
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
     }).then(response => response.json())
     setTrackingData(data)
-}
+  }
   return (
     <>
-    <div className="flex flex-col justify-center bg-transparent items-center ">
-      {/*<div className="bg-white p-4 rounded-lg shadow-lg">*/}
-        
+      <div className="flex flex-col justify-center bg-transparent items-center ">
+        {/*<div className="bg-white p-4 rounded-lg shadow-lg">*/}
+
         <form className="flex" onSubmit={handleSubmit}>
           <input required
             name="awb"
             value={formData.id}
             onChange={handleChange}
-            type="text" 
-            placeholder="Enter tracking number" 
-            className="text-6px w-52 md:w-64 p-3 shadow-lg shadow-gray-400 focus:outline-none rounded-l-lg focus:border-white  text-gray-900" 
+            type="text"
+            placeholder="Enter tracking number"
+            className="text-6px w-52 md:w-64 p-3 shadow-lg shadow-gray-400 focus:outline-none rounded-l-lg focus:border-white  text-gray-900"
           />
-          <button 
-            type="submit" 
+          <button
+            type="submit"
             className="bg-sky-800 text-gray-300 text-bold p-3 rounded-r-lg hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
           >
             Track
@@ -131,8 +133,8 @@ const handleSubmit = async (e) => {
 
       </div>
       {trackingData && <Result data={trackingData} />}
-      </>
-    
+    </>
+
   );
 };
 

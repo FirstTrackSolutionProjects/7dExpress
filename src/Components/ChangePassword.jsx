a0dc06ae7bb45fb7381fa85b1c48bb7341e06a40
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-01-23 13:16:58 +0530

    chore : applied hideable password at change password

--- src/Components/ChangePassword.jsx
+++ src/Components/ChangePassword.jsx
@@ -1,4 +1,6 @@
+import { Box, Button, TextField } from '@mui/material'
 import React, { useState } from 'react'
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 const API_URL = import.meta.env.VITE_APP_API_URL
 const ChangePassword = () => {
     const INITIAL_STATE = {
@@ -6,6 +8,9 @@ const ChangePassword = () => {
         newPassword : '',
         confirmNewPassword : ''
     }
+    const [showOldPassword, setShowOldPassword] = useState(false);
+    const [showNewPassword, setShowNewPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const [formData, setFormData] = useState(INITIAL_STATE)
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -35,10 +40,49 @@ const ChangePassword = () => {
       <div className='w-full p-8 flex flex-col items-center'>
       <div className='text-center text-3xl font-medium text-black mb-8'>Change Password</div>
       <form action="" onSubmit={handleSubmit} className="w-full sm:w-auto flex px-3 flex-col mt-3 space-y-3 sm:space-y-5 text-black">
-          <input type="password" placeholder="Old Password" value={formData.oldPassword} onChange={handleChange} name="oldPassword" className="py-2 px-3 rounded-xl w-full sm:w-[400px]" />
-          <input type="password" placeholder="New Password" name="newPassword" value={formData.newPassword} onChange={handleChange} className="py-2 px-3 rounded-xl  w-full sm:w-[400px]"/>
-          <input type="password" placeholder="Confirm New Password" name="confirmNewPassword" value={formData.confirmNewPassword} onChange={handleChange} className="py-2 px-3 rounded-xl  w-full sm:w-[400px]"/>
-          <button type="submit" className="py-2 px-3 rounded-xl  w-full sm:w-[400px] border border-black  hover:bg-[rgba(135,206,235,1)]">Change password</button>
+          <TextField 
+              type={showOldPassword?'text':'password'} 
+              size='small'
+              InputProps={{
+                endAdornment: <Box className="h-4 cursor-pointer" onClick={()=>setShowOldPassword((prev)=>!prev)}>
+                  {!showOldPassword?<FaEye/>:<FaEyeSlash/>}
+                </Box>
+              }} 
+              placeholder="Old Password" 
+              value={formData.oldPassword} 
+              onChange={handleChange} 
+              name="oldPassword" 
+              className="py-2 px-3 rounded-xl w-full sm:w-[400px]" 
+            />
+            <TextField 
+              type={showNewPassword?'text':'password'} 
+              size='small'
+              InputProps={{
+                endAdornment: <Box className="h-4 cursor-pointer" onClick={()=>setShowNewPassword((prev)=>!prev)}>
+                  {!showNewPassword?<FaEye/>:<FaEyeSlash/>}
+                </Box>
+              }} 
+              placeholder="New Password" 
+              value={formData.newPassword} 
+              onChange={handleChange} 
+              name="newPassword"  
+              className="py-2 px-3 rounded-xl w-full sm:w-[400px]" 
+            />
+            <TextField 
+              type={showConfirmPassword?'text':'password'} 
+              size='small'
+              InputProps={{
+                endAdornment: <Box className="h-4 cursor-pointer" onClick={()=>setShowConfirmPassword((prev)=>!prev)}>
+                  {!showConfirmPassword?<FaEye/>:<FaEyeSlash/>}
+                </Box>
+              }} 
+              placeholder="Confirm New Password" 
+              value={formData.confirmNewPassword} 
+              onChange={handleChange} 
+              name="confirmNewPassword" 
+              className="py-2 px-3 rounded-xl w-full sm:w-[400px]" 
+            />
+          <Button type="submit" variant='contained' disabled={!formData.oldPassword || !formData.newPassword || !formData.confirmNewPassword}>Change password</Button>
         </form>
       </div>
     </div>

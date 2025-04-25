1cf81f08bad21dd6e4e762572f2b389bec6a271d
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-04-25 16:29:05 +0530

    chore: announcement

--- src/Components/AnnoucementCard.jsx
+++ src/Components/AnnoucementCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AnnoucementCard = () => {
   return (
     <div className='px-4 pb-4 pt-8 bg-blue-500 m-5 rounded-md text-white relative'>
-      <div className='absolute top-1 right-1'>Updated at :- 26-03-2025</div>
+      <div className='absolute top-1 right-1'>Updated at :- 25-04-2025</div>
          DTDC B2C SERVICE IS DISABLED DUE TO TECHNICAL ISSUE. SORRY FOR YOUR INCONVENIENCE.
     </div>
   )

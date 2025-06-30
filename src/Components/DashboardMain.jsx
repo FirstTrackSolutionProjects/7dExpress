0de6cace4162318caf84dc27e6145e84fdf99e9a
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-06-30 21:19:55 +0530

    feat: added owner annoucement

--- src/Components/DashboardMain.jsx
+++ src/Components/DashboardMain.jsx
@@ -1,12 +1,15 @@
 // import DashboardStatement from "./DashboardStatement"
+import { useAuth } from "../context/AuthContext"
 import AnnoucementCard from "./AnnoucementCard"
 import DashboardSummary from "./DashboardSummary"
 import Profile from "./Profile"
 // import RazorpayPayment from "./Razorpay"
 const DashboardMain = () => {
+  const { admin } = useAuth();
   return (
     <div className=" py-16 w-full h-full flex flex-col items-center overflow-x-hidden overflow-y-auto">
       {/* <Profile /> */}
+      {admin ? <OwnerAnnoncement /> : null}
       <AnnoucementCard />
       <DashboardSummary />
       {/* <DashboardStatement /> */}
---
+++ src/Components/OwnerAnnoncement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OwnerAnnoncement = () => {
+  return (
+    <div className='px-4 pb-4 pt-8 bg-blue-500 m-5 rounded-md text-white relative'>
+      <div className='absolute top-1 right-1'>Updated at :- 30-06-2025</div>
+         7dexpress.com is expiring on 4th July 2025, please reach out for the further information before 3rd July 2025.
+    </div>
+  )
+}
+
+export default OwnerAnnoncement

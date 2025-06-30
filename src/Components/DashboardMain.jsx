cd3790502f6fb820c1b908fbb42b84573236925c
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-06-30 21:22:26 +0530

    feat: added owner annoucement

--- src/Components/DashboardMain.jsx
+++ src/Components/DashboardMain.jsx
@@ -2,6 +2,7 @@
 import { useAuth } from "../context/AuthContext"
 import AnnoucementCard from "./AnnoucementCard"
 import DashboardSummary from "./DashboardSummary"
+import OwnerAnnoncement from "./OwnerAnnoncement"
 import Profile from "./Profile"
 // import RazorpayPayment from "./Razorpay"
 const DashboardMain = () => {

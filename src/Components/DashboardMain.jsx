8e719852b984a700699189520c82d027304e59f8
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-08-06 19:43:52 +0530

    chore: removed announcement

--- src/Components/DashboardMain.jsx
+++ src/Components/DashboardMain.jsx
@@ -10,8 +10,8 @@ const DashboardMain = () => {
   return (
     <div className=" py-16 w-full h-full flex flex-col items-center overflow-x-hidden overflow-y-auto">
       {/* <Profile /> */}
-      {admin ? <OwnerAnnoncement /> : null}
-      <AnnoucementCard />
+      {/* {admin ? <OwnerAnnoncement /> : null} */}
+      {/* <AnnoucementCard /> */}
       <DashboardSummary />
       {/* <DashboardStatement /> */}
       {/* <RazorpayPayment/> */}

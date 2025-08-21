e419be9668c3271c5c8e705decbf6c97e0e6c57b
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-08-21 14:52:09 +0530

    feat: added delhiveryB2B option in pickup request

--- src/Components/UpdateOrder.jsx
+++ src/Components/UpdateOrder.jsx
@@ -1225,8 +1225,9 @@ const PickupRequest = ({ setPickup }) => {
               label="Delivery Partner"
             >
               <MenuItem value="">Select Service</MenuItem>
-              <MenuItem value="2">Delhivery (10Kg)</MenuItem>
-              <MenuItem value="1">Delhivery (500gm)</MenuItem>
+              <MenuItem value="1">Delhivery B2B</MenuItem>
+              <MenuItem value="2">Delhivery (500gm)</MenuItem>
+              <MenuItem value="3">Delhivery (10Kg)</MenuItem>
             </Select>
           </FormControl>
           

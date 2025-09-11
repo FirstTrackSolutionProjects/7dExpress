4e5e211b5c75113e57928cd2980ff894029d88fb
adyaananyamishra <adyaananyamishra@email.com>
2025-09-11 16:54:44 +0530

    chore: added WP

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -139,7 +139,7 @@ const Contact = () => {
       </div>
       { /* Whatsapp */}
        <a
-        href="https://wa.me/919876543210"
+        href="https://wa.me/919082974657"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg flex items-center justify-center z-50 transition-colors"
